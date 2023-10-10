d26e22a4aac793328f442d55eb0b32ec9d313fb1
evingerbono <bono0823@gmail.com>
2023-10-10 09:55:29 +0200

    szomszéd_Kapcsolása

--- Model/Model.js
+++ Model/Model.js
@@ -1,13 +1,28 @@
 
 class Model{
     #lista=[];
+    #allapot;
+
+    constructor(){
+        this.#allapot=false;
+        for (let index = 1; index <= 9; index++) {
+            this.#lista.push(this.#allapot);
+        }
+    }
+    setSzin(id){
+        $(`#${id}`).css("background-color","red");
+        this.#lista[id]=true;
+        console.log(this.#lista);
+        for (let index = 0; index <= 9; index++) {
+            if (this.#lista[index]!==true) {
+                $(`#${id+1}`).css("background-color","blue");
+                $(`#${id-1}`).css("background-color","blue");
+            }
+        }
+    }
 
     getList(){
         return this.#lista;
-    }
-
-    setSzin(id){
-        $(`#${id}`).css("background-color","red");
     }
 }
 export default Model;
